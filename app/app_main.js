13da22a37d5ef20f0bb5fd1fb5ec4cbc1c032948
shannsoft <santoshmajhi99@gmail.com>
2015-02-27 20:34:56 +0530

    --Changed for module name

--- app/app_main.js
+++ app/app_main.js
@@ -1,5 +1,5 @@
 // Define all your modules with no dependencies
-angular.module('birthday', []);
+angular.module('home', []);
 
 // Lastly, define your "main" module and inject all other modules as dependencies
 var AppRoot = angular.module('appMain',['ionic','home']);
