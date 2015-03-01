b4ca43ce2474a36be3040297803a81c4faf34d9e
Rajendra <cet.rajendra2010@gmail.com>
2015-03-01 09:07:23 +0530

    -Added grunt file for minify

--- app/modules/home/controllers/controller.js
+++ app/modules/home/controllers/controller.js
@@ -1,6 +1,6 @@
 angular.module("home").controller("homeController",['$scope','$rootScope','HomeService', function($scope,$rootScope,HomeService){
    HomeService.getCountries().then(function(pRes){
-   console.log(pRes.data);
-
+   	console.log(pRes.data);
+   	
    });
 }]);
