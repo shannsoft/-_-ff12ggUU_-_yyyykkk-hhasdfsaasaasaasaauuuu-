e648d3ae8eaac75abcb9ac990357a1eb7b35b41b
Ashutosh-ionic <ashutosh.mahanta@gmail.com>
2015-03-01 18:52:27 +0530

    --add json format

--- app/modules/travel/controllers/controller.js
+++ app/modules/travel/controllers/controller.js
@@ -6,4 +6,6 @@ angular.module("travel").controller("travelController",['$scope','$rootScope', f
   		{detailLink:"#traffic-information",iconClass:"fa fa-plane",info:"Traffic Mobility Plan"},
   		{detailLink:"#fuel-pump",iconClass:"fa fa-tachometer",info:"Fuel Pump"},
   	];
-}]);
+
+  	$scope.trainInfo = '{"trains" : [{"TrainID": "1","TrainNumber" : "47544","TrainName" : "Dhauli Express","FromStation" : "Puri","ToStation" : "Bhubaneswar","StartAt" : "10:00","ReachesAt" : "11:30","Availabilty" : "["sunday", "tuesday","friday"]"},{"TrainID": "2","TrainNumber" : "47588","TrainName" : "Neel Express","FromStation" : "Hydradabad","ToStation" : "Puri","StartAt" : "18:00","ReachesAt" : "6:30","Availabilty" : "["sunday", "tuesday","friday"]"}]}';
+  }]);
