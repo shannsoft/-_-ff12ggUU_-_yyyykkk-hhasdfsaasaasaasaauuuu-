f5cca5ef8da94667c3d625a17deae571252b24e6
Ashutosh-ionic <ashutosh.mahanta@gmail.com>
2015-03-01 15:51:51 +0530

    change some module

--- app/modules/home/controllers/controller.js
+++ app/modules/home/controllers/controller.js
@@ -1,6 +1,14 @@
 angular.module("home").controller("homeController",['$scope','$rootScope','HomeService', function($scope,$rootScope,HomeService){
-   HomeService.getCountries().then(function(pRes){
-   	console.log(pRes.data);
-   	
-   });
-}]);
+  $scope.MenuOptionList = [
+  		{detailLink:"index.html",icon:"fa fa-qrcode",info:"Dashboard"},
+  		{detailLink:"#travel",icon:"fa fa-plane",info:"Travel"},
+  		{detailLink:"#stay",icon:"fa fa-home",info:"Stay"},
+  		{detailLink:"#money",icon:"fa fa-inr",info:"Money"}, 				
+  		{detailLink:"#events",icon:"fa fa-users",info:"Events"}, 				
+  		{detailLink:"parking.html",icon:"fa fa-taxi",info:"Parking"}, 				
+  		{detailLink:"contacts.html",icon:"fa fa-book",info:"Emergency Contact"}, 				
+  		{detailLink:"route-map.html",icon:"fa fa-map-marker",info:"Route Map"}, 				
+  		{detailLink:"template-schedule.html",icon:"fa fa-file-text",info:"About Nabkalebara"}, 				
+  		{detailLink:"template-schedule.html",icon:"fa fa-calendar",info:"Temple Schedule"}, 				
+  	];
+}]);	
