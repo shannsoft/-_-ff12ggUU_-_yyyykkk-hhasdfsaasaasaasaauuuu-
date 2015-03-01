c15c2d971adf36994015a25be221fe485b46a289
Rajendra <cet.rajendra2010@gmail.com>
2015-03-01 09:55:55 +0530

    - grunt modified

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
       separator: ';',
     },
     dist: {
-      src: 'app/modules/*.js',
+      src: ['app/modules/**/*.js',],
       dest: 'libenc.js',
     },
   }
