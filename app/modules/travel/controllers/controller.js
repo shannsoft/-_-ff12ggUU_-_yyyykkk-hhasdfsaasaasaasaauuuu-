5c4a93acc9808d8b37fbca7a90c048fe0fe43587
Ashutosh-ionic <ashutosh.mahanta@gmail.com>
2015-03-02 21:55:00 +0530

    --Added train details

--- app/modules/travel/controllers/controller.js
+++ app/modules/travel/controllers/controller.js
@@ -7,5 +7,113 @@ angular.module("travel").controller("travelController",['$scope','$rootScope', f
   		{detailLink:"#fuel-pump",iconClass:"fa fa-tachometer",info:"Fuel Pump"},
   	];
 
-  	$scope.trainInfo = '{"trains" : [{"TrainID": "1","TrainNumber" : "47544","TrainName" : "Dhauli Express","FromStation" : "Puri","ToStation" : "Bhubaneswar","StartAt" : "10:00","ReachesAt" : "11:30","Availabilty" : "["sunday", "tuesday","friday"]"},{"TrainID": "2","TrainNumber" : "47588","TrainName" : "Neel Express","FromStation" : "Hydradabad","ToStation" : "Puri","StartAt" : "18:00","ReachesAt" : "6:30","Availabilty" : "["sunday", "tuesday","friday"]"}]}';
-  }]);
+  	/*
+	This is starting control for train information partial
+  	*/
+    
+  	$scope.trainDetails = [];
+  	$scope.trainInfo = '{'+
+    '"trains": ['+
+        '{'+
+            '"TrainID": "1",'+
+            '"TrainNumber": "47544",'+
+            '"TrainName": "Dhauli Express",'+
+            '"FromStation": "Puri",'+
+            '"ToStation": "Bhubaneswar",'+
+            '"StartAt": "10:00",'+
+            '"ReachesAt": "11:30",'+
+            '"Availabilty": ['+
+                '"sunday",'+
+                '"tuesday",'+
+                '"friday"'+
+            ']'+
+        '},'+
+        '{'+
+            '"TrainID": "2",'+
+            '"TrainNumber": "47577",'+
+            '"TrainName": "Neel Express",'+
+            '"FromStation": "Hyderabad",'+
+            '"ToStation": "Puri",'+
+            '"StartAt": "09:15",'+
+            '"ReachesAt": "18:05",'+
+            '"Availabilty": ['+
+                '"sunday",'+
+                '"monday",'+
+                '"friday"'+
+            ']'+
+        '},'+
+        '{'+
+            '"TrainID": "1",'+
+            '"TrainNumber": "47544",'+
+            '"TrainName": "Dhauli Express",'+
+            '"FromStation": "Puri",'+
+            '"ToStation": "Bhubaneswar",'+
+            '"StartAt": "10:00",'+
+            '"ReachesAt": "11:30",'+
+            '"Availabilty": ['+
+                '"sunday",'+
+                '"tuesday",'+
+                '"friday"'+
+            ']'+
+        '},'+
+        '{'+
+            '"TrainID": "2",'+
+            '"TrainNumber": "47577",'+
+            '"TrainName": "Neel Express",'+
+            '"FromStation": "Hyderabad",'+
+            '"ToStation": "Puri",'+
+            '"StartAt": "09:15",'+
+            '"ReachesAt": "18:05",'+
+            '"Availabilty": ['+
+                '"sunday",'+
+                '"monday",'+
+                '"friday"'+
+            ']'+
+        '},'+
+        '{'+
+            '"TrainID": "1",'+
+            '"TrainNumber": "47544",'+
+            '"TrainName": "Dhauli Express",'+
+            '"FromStation": "Puri",'+
+            '"ToStation": "Bhubaneswar",'+
+            '"StartAt": "10:00",'+
+            '"ReachesAt": "11:30",'+
+            '"Availabilty": ['+
+                '"sunday",'+
+                '"tuesday",'+
+                '"friday"'+
+            ']'+
+        '},'+
+        '{'+
+            '"TrainID": "2",'+
+            '"TrainNumber": "47577",'+
+            '"TrainName": "Neel Express",'+
+            '"FromStation": "Hyderabad",'+
+            '"ToStation": "Puri",'+
+            '"StartAt": "09:15",'+
+            '"ReachesAt": "18:05",'+
+            '"Availabilty": ['+
+                '"sunday",'+
+                '"monday",'+
+                '"friday"'+
+            ']'+
+        '}]}';
+
+	var parseData = JSON.parse($scope.trainInfo);
+	$(parseData.trains).each(function(i){
+   
+    var trainDetails = {};
+    trainDetails.trainName = parseData.trains[i].TrainName;
+    trainDetails.trainNumber = parseData.trains[i].TrainNumber;
+    trainDetails.startAt = parseData.trains[i].StartAt;
+    trainDetails.reachesAt = parseData.trains[i].ReachesAt;
+    trainDetails.fromStation = parseData.trains[i].FromStation;
+    trainDetails.toStation = parseData.trains[i].ToStation;
+		trainDetails.availability = parseData.trains[i].Availabilty;
+    $scope.trainDetails.push(trainDetails);
+	});
+	/*
+	This is END control for train information partial
+  	*/
+
+}]);
