585189515b9edda1ed98ab64140c7a3fc8fe09a7
shannsoft <santoshmajhi99@gmail.com>
2015-03-18 17:30:03 +0530

    add map

--- app/modules/home/controllers/controller.js
+++ app/modules/home/controllers/controller.js
@@ -1,22 +1,76 @@
-angular.module("home").controller("homeController",['$scope','$rootScope','MainService','AppModelService','MainEvent', function($scope,$rootScope,MainService,AppModelService,MainEvent){
+angular.module("home").controller("homeController",['$scope','$rootScope','MainService','AppModelService','MainEvent','HomeService', function($scope,$rootScope,MainService,AppModelService,MainEvent,HomeService){
+  var map;
+  var infowindow;
   $scope.homeInit = function(){
-    console.log("homeInit");
-    $scope.contentUrl = 'modules/home/views/partials/mainMenu.html';
+    $scope.contentUrl = HomeService.getContentUrl();
+    //HomeService.setContentUrl('modules/home/views/partials/mainMenu.html');
     $scope.heading = 'Home';
     $scope.menuOptionList = AppModelService.getMenuOptions();
   }
-  $scope.changeContentUrl = function(contentURL,link){
-    $scope.contentUrl = contentURL;
-    window.location = link; 
-    console.log(contentURL,link);
-  }
-
   $rootScope.showNoty = function(){
     alert(0);
   }
-  $rootScope.$on(MainEvent.INIT_MAP,function(event,pRes){
+  $scope.changeContentUrl = function(pUrl){
+    $scope.contentUrl = pUrl;
+     HomeService.setContentUrl(pUrl);
 
-       console.log("init map for "+pRes.data);
+  }
+  $rootScope.$on(MainEvent.INIT_MAP,function(event,pRes){
+       window.location = '#/';
+       $scope.heading = 'Home';
+      HomeService.setContentUrl('modules/home/views/partials/map-view.html');
   });
+
+  $scope.geoFindMe = function(){
+    if (!navigator.geolocation){
+      output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+      return;
+    }
+    function success(position) {
+      var latitude  = position.coords.latitude;
+      var longitude = position.coords.longitude;
+      $scope.foundOnMap(latitude,longitude);
+    };
+    function error() {
+      output.innerHTML = "Unable to retrieve your location";
+    };
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+  $scope.foundOnMap = function(lat,lan) {
+    var pyrmont = new google.maps.LatLng(lat, lan);
+    map = new google.maps.Map(document.getElementById('map-canvas'), {
+      center: pyrmont,
+      zoom: 15
+    });
+
+    var request = {
+      location: pyrmont,
+      radius: 2000,
+      types: ['atm']
+    };
+
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);
+  }
+  function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        createMarker(results[i]);
+      }
+    }
+  }
+
+  function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+      map: map,
+      position: place.geometry.location
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.setContent(place.name);
+      infowindow.open(map, this);
+    });
+  }
 
 }]);	
--- app/modules/home/services/service.js
+++ app/modules/home/services/service.js
@@ -1,8 +1,15 @@
-AppRoot.factory('AboutUsService',['$http','AppConfig','$rootScope', function($http,AppConfig,$rootScope) {
+AppRoot.factory('HomeService',['$http','AppConfig','$rootScope', function($http,AppConfig,$rootScope) {
+    var contentUrl = 'modules/home/views/partials/mainMenu.html';
     return{
     	getCountries : function(){
     		var response = $http.get(AppConfig.apiPath+"reqmethod=getSchedule");
     		return response;
+    	},
+    	setContentUrl : function(val){
+    		contentUrl = val;
+    	},
+    	getContentUrl : function(){
+    		return contentUrl;
     	}
     }
 }]);
