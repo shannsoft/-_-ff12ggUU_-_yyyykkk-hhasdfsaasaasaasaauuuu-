7079c98b337f509088f28a110229dae07f10362b
rajendradbpb <rajendrasahoodbpb@gmail.com>
2015-03-25 06:42:07 -0500

    - mapping of about nabakalebara from side menu added

--- app/services/app_model_service.js
+++ app/services/app_model_service.js
@@ -13,8 +13,8 @@ AppRoot.factory('AppModelService',['$http','AppConfig','$rootScope', function($h
         {detailLink:"#parking",icon:"fa fa-taxi",info:"Parking"},         
         {detailLink:"#contacts",icon:"fa fa-book",info:"Emergency Contact"},        
         {detailLink:"#route-map",icon:"fa fa-map-marker",info:"Route Map"},         
-        {detailLink:"template-schedule.html",icon:"fa fa-file-text",info:"About Nabkalebara"},        
-        {detailLink:"template-schedule.html",icon:"fa fa-calendar",info:"Temple Schedule"},         
+        {detailLink:"#aboutUs",icon:"fa fa-file-text",info:"About Nabkalebara"},        
+        {detailLink:"#template-schedule",icon:"fa fa-calendar",info:"Temple Schedule"},         
     ]
     getMenuOptions = function(){
          return menuOptionList;
